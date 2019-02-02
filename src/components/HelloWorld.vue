1c256f72dfc22b2266d3f33168568e0788285599
Farhan Fauzan <farhanfauzan170@gmail.com>
2019-02-02 23:15:30 +0700

    quizvue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,133 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 v-if="show">{{ msg }}</h1>
+    <h1 v-else>Judul Disembunyikan</h1>
+    <p>Skor : {{skor}}/3</p>
+    <p>{{ nama}}</p>
+    <input type="text" v-model="nama">
+    <button type="button" v-on:click="hideTitle">Click Me</button>
+    <br>
+    <!--SOAL1-->
+    <li v-for="s in soal">
+    <h3>{{s}}</h3>
+    <jawaban1 v-for="j in jawaban1">
+    <br>
+    <input type="radio" v-on:click="increment" value="A. Dont look back in anger">
+    {{j}}
+    </jawaban1>
+    <br>
+    <jawaban2 v-for="a in jawaban2">
+    <input type="radio" v-on:click="increment" value="B. Perfect">
+    {{a}}
+    </jawaban2>
+    <br>
+    <jawaban3 v-for="w in jawaban3">
+    <input type="radio" v-on:click="increment" value="C. SAD!">
+    {{w}}
+    </jawaban3>
+    <br>
+    <jawaban4 v-for="b in jawaban4">
+    <input type="radio" v-on:click="increment" value="D. Cinta di pantai bali">
+    {{b}}
+    </jawaban4>
+    <!--SOAL2-->
+      <li v-for="o in soal2">
+    <h3>{{o}}</h3>
+    <option1 v-for="p in option1">
+    <br>
+    <input type="radio" v-on:click="increment" value="A. China">
+    {{p}}
+    </option1>
+    <br>
+    <option2 v-for="t in option2">
+    <input type="radio" v-on:click="increment" value="B. Russia">
+    {{t}}
+    </option2>
+    <br>
+    <option3 v-for="i in option3">
+    <input type="radio" v-on:click="increment" value="C. Phillipina">
+    {{i}}
+    </option3>
+    <br>
+    <option4 v-for="n in option4">
+    <input type="radio" v-on:click="increment" value="D. Brazil">
+    {{n}}
+    </option4>
+     <!--SOAL3-->
+      <li v-for="l in soal3">
+    <h3>{{l}}</h3>
+    <choose1 v-for="c in choose1">
+    <br>
+    <input type="radio" v-on:click="increment" value="A. Ronaldo">
+    {{c}}
+    </choose1>
+    <br>
+    <choose2 v-for="e in choose2">
+    <input type="radio" v-on:click="increment" value="B. Messi">
+    {{e}}
+    </choose2>
+    <br>
+    <choose3 v-for="s in choose3">
+    <input type="radio" v-on:click="increment" value="C. Fabregas">
+    {{s}}
+    </choose3>
+    <br>
+    <choose4 v-for="h in choose4">
+    <input type="radio" v-on:click="increment" value="D. Ronaldowati">
+    {{h}}
+    </choose4>
+    </li>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data : function() {
+  return{
+    nama: 'Nouvan',
+    show: true,
+    skor: 0,
+    soal : ['1. Lagu apa yang dinyanyikan oleh ed sheeran ?'],
+    jawaban1 : ['A. Dont look back in anger'],
+    jawaban2 : ['B. Perfect'],
+    jawaban3 : ['C. SAD!'],
+    jawaban4 : ['D. Cinta di pantai bali'],
+    soal2 : ['2. Negara paling padat penduduknya ?'],
+    option1 : ['A. China'],
+    option2 : ['B. Russia'],
+    option3 : ['C. Phillipina'],
+    option4 : ['D. Brazil'],
+    soal3 : ['3. Siapa pemain terkenal di real madrid?'],
+    choose1 : ['A. Ronaldo'],
+    choose2 : ['B. Messi'],
+    choose3 : ['C. Fabregas'],
+    choose4 : ['D. Ronaldowati']
+    }
+    },
+  methods: {
+    hideTitle() {
+      this.show = !this.show
+    },
+    increment() {
+    var value = event.target.value;
+    if (value === "B. Perfect"){
+    this.skor++;
+    }else if (value === "A. China"){
+    this.skor++;
+    }else if (value === "A. Ronaldo"){
+    this.skor++;
+    }else{
+			this.skor--;
+		}
+    }
+  },
+  mounted: function () {
+    this.nama = 'Nouvan Rachmad'
   }
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
